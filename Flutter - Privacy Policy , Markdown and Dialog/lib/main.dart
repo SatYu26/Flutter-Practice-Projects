f39a148d10f7689bc06d1975349de7a44316addb
SatYu26 <goyalsatyam8@gmail.com>
2021-01-23 19:20:08 +0530

    Privacy Policy, markdown and dialogs

---
+++ Flutter - Privacy Policy , Markdown and Dialog/lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:project001/src/pages/sign_in_page.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: SignInPage(),
+    );
+  }
+}
