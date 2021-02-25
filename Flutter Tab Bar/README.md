7651527ca846ae3a0d0db8c23195875118b141e8
SatYu26 <goyalsatyam8@gmail.com>
2021-02-25 18:25:26 +0530

    Tab Bar Demo

---
+++ Flutter Tab Bar/README.md
@@ -0,0 +1,7 @@
+# flutter_tabbar
+
+Flutter Tab Bar Demo.
+
+## Getting Started
+
+https://blog.logrocket.com/flutter-tabbar-a-complete-tutorial-with-examples/
---
+++ Flutter Tab Bar/lib/main.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'TabBar Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.grey,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'TabBar Demo'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          bottom: TabBar(
+            indicatorColor: Colors.amberAccent,
+            // indicatorSize: TabBarIndicatorSize.label,
+            // indicatorWeight: 10,
+            indicator: BoxDecoration(
+                borderRadius: BorderRadius.circular(50), // Creates border
+                color: Colors.greenAccent),
+            tabs: [
+              Tab(icon: Icon(Icons.flight)),
+              Tab(icon: Icon(Icons.directions_transit)),
+              Tab(icon: Icon(Icons.directions_car)),
+            ],
+          ),
+          title: Center(child: Text('Tabs Demo')),
+        ),
+        body: TabBarView(
+          children: [
+            Icon(Icons.flight, size: 350),
+            Icon(Icons.directions_transit, size: 350),
+            Icon(Icons.directions_car, size: 350),
+          ],
+        ),
+      ),
+    );
+  }
+}
